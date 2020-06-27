e2b4882b0d61f2e1c7bb8160eb2da588e8b04549
Anshuman301 <anshumanprsd@gmail.com>
2020-06-27 20:46:31 +0530

    Updates

---
+++ precache-manifest.71d51c246fd1a0fd01458bbc60850b05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239d49a9015a7e89ab7f04de6c1ca55b",
+    "url": "/insta-app/index.html"
+  },
+  {
+    "revision": "46dca9d116add39f9833",
+    "url": "/insta-app/static/css/main.1a190928.chunk.css"
+  },
+  {
+    "revision": "6de93f881dadaa141995",
+    "url": "/insta-app/static/js/2.b91d5b6f.chunk.js"
+  },
+  {
+    "revision": "46dca9d116add39f9833",
+    "url": "/insta-app/static/js/main.c084c990.chunk.js"
+  },
+  {
+    "revision": "7d6acba24952026ebe0e",
+    "url": "/insta-app/static/js/runtime~main.dae12fc9.js"
+  },
+  {
+    "revision": "902c4365b6e050acc85eab3d8c238d94",
+    "url": "/insta-app/static/media/explore.902c4365.svg"
+  },
+  {
+    "revision": "0ed5fbdb544618b2919b98c91fe23108",
+    "url": "/insta-app/static/media/home.0ed5fbdb.svg"
+  },
+  {
+    "revision": "bb026c4d996a5d563f7fceb5166175e1",
+    "url": "/insta-app/static/media/instagram.bb026c4d.svg"
+  },
+  {
+    "revision": "c05f2fa37a7d7705106322c6fa1aab54",
+    "url": "/insta-app/static/media/user.c05f2fa3.svg"
+  }
+]);
